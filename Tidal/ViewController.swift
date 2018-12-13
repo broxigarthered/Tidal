9d0782e28f8e69c49d135202368fd1856a69f7f7
NikiMusala <nikolay.dutskinov@musala.com>
2018-12-13 15:04:01 +0200

    Initial Commit

---
+++ Tidal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tidal
+//
+//  Created by Nikolay N. Dutskinov on 13/12/2018.
+//  Copyright © 2018 Nikolay N. Dutskinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
